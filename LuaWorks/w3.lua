0ddcf44eccc205e74d4a14f9251d02bf7fcaef29
apr.sf75 <apr.sf75@reflexiverabbit.com>
2011-09-23 21:23:16 +0800

    finished w3

--- LuaWorks/w3.lua
+++ LuaWorks/w3.lua
@@ -10,18 +10,24 @@ local function leapYear(theYear)
 	-- your calcuation here
 
 	-- e.g.
-	if (theYear == 4) then
-		return true
-	else 
+	if (theYear % 4)==0 then
+		if (theYear % 100)==0 then
+			if(theYear % 400)==0 then
+				return true -- 400, 
+			else
+				return false
+			end
+		else
+			return true
+		end
+	else
 		return false
 	end
-
 end
-
 run = function()
    -- call your function on year xxxx
-	local year1 = 1984
-	local result1 = leapYear(year1)
+	local year1 = 2004
+	local result = leapYear(year1)
 	if (result == true) then
 		WorkPad1:setText("year " .. year1 .. " is leap ")
 	else
