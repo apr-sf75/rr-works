cd61048bfd74a4ae9a04f4f78c8ab2e6623b0587
apr.sf75 <apr.sf75@reflexiverabbit.com>
2011-10-16 10:43:44 +0800

    Finished hanoi works

--- HanoiWorks/main.lua
+++ HanoiWorks/main.lua
@@ -41,7 +41,24 @@ local function setup()
 	plate.isFixedRotation = true 
 	plate.myName = "plate"
 
+
+	local plate1 = display.newImage( "plate1.png" )
+	plate1.x = 80; plate.y = 200
+	physics.addBody( plate1, { density=1.0, bounce=0.2 } )
+	plate1.isFixedRotation = true 
+	plate1.myName = "plate1"
+
+	local plate2 = display.newImage( "plate2.png" )
+	plate2.x = 80; plate.y = 200
+	physics.addBody( plate2, { density=1.0, bounce=0.2 } )
+	plate2.isFixedRotation = true 
+	plate2.myName = "plate2"
+
+	plate2:addEventListener( "touch", hanoiDrag.startDrag )
+
+
 	-- Add touch event listeners to objects
+	plate1:addEventListener( "touch", hanoiDrag.startDrag )
 	plate:addEventListener( "touch", hanoiDrag.startDrag )
 end
 
