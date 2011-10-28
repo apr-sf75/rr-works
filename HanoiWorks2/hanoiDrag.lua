4530bf2d2a6cbb14889af9bb1cb54c6bed36f783
apr.sf75 <apr.sf75@reflexiverabbit.com>
2011-10-28 17:28:04 +0800

    finished hanoi homework

--- HanoiWorks2/hanoiDrag.lua
+++ HanoiWorks2/hanoiDrag.lua
@@ -35,13 +35,25 @@ local function isTopPlate(plate)
 	end
 end
 
+
 local function isSmallerThanTop(plateId, stickIdx)
-	-- homework
+	
 	
 	-- check corner case, empty stick?
+	local theStick = gSticks[stickIdx]
+	local thePlates = theStick.plates
+
+	if #theStick.plates == 0 then
+		return true
+	end
+
 	
 	-- check normal case, get the top plate id and compare with it
-	
+	local theTopPlateId = thePlates[#thePlates]
+	if plateId < theTopPlateId then
+		return true
+	end
+    return false
 end
 
 local function movePlateFromStickToStick(plateId, fromStickIdx, toStickIdx)
@@ -67,23 +79,34 @@ end
 -- another to freeze all other plates during dragging
 -- this is also preventing player from corrupting the game by invalid drag
 local function activateTopPlates()
-	-- bonus homework	
+
 end
 
 local function freezeOtherPlate(plateId)
-	-- bonus homework
+
 end
 
 local function isGameFinished()
-	-- homework
+
+
+	
 	-- conditions:
+	if (#gSticks[1].plates == 0 and #gSticks[2].plates == 0) or (#gSticks[1].plates == 0 and #gSticks[3].plates == 0) then
+		return true
+	end
 	-- stick 1 is empty
+		
 	-- either either 2 or either 3 is empty, i.e. all plates moved
 end
 
 local function bestNumberOfStepsForPlates(numOfPlates)
 	-- calculate the best number
-	-- homework
+
+	
+
+	
+
+
 	-- formula, bestMove (n) =  bestMove(n-1) + 1 + bestMove(n-1) 
 	-- i.e. bestMove is by 
 		-- moving plate1..plate(N-1) to stick 2, need bestMove(N-1) steps
@@ -92,7 +115,8 @@ local function bestNumberOfStepsForPlates(numOfPlates)
 	-- and bestMove(1) = 1
 	
 	-- example answer for 3 plates
-	return 7
+	return math.pow(2,numOfPlates) - 1
+
 end
 
 local function isBestMovement()
